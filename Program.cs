24233be89043cf7a4ef37538115e80d77b984667
willpadian <wpadian2023@chadwickschool.org>
2022-02-17 11:19:35 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
